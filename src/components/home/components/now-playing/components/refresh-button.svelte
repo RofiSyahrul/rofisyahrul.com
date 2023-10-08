4fd5f28edc40a469ac22616f5f9105a30834e3f1
RofiSyahrul <srofi44@gmail.com>
2023-10-08 19:45:47 +0700

    feat: recreate now-playing widget

---
+++ src/components/home/components/now-playing/components/refresh-button.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Refresh from '@/shared/icons/refresh.svelte';
+</script>
+
+<button
+  class="btn btn-text btn-secondary h-8 z-[1]"
+  title="Refresh"
+  on:click
+>
+  <Refresh />
+  <span class="visually-hidden">Refresh</span>
+</button>
---
+++ src/shared/types/spotify.ts
@@ -0,0 +1,77 @@
+export interface SpotifyExternalURL {
+  spotify: string;
+}
+
+export interface SpotifyImage {
+  height: number;
+  url: string;
+  width: number;
+}
+
+export interface SpotifyAlbum {
+  album_type: 'album' | 'single' | 'compilation';
+  external_urls: SpotifyExternalURL;
+  id: string;
+  images: SpotifyImage[];
+  name: string;
+}
+
+export interface SpotifyArtist {
+  external_urls: SpotifyExternalURL;
+  id: string;
+  name: string;
+}
+
+export interface SpotifyTrack<TPreview = string | null> {
+  album: SpotifyAlbum;
+  artists: SpotifyArtist[];
+  duration_ms: number;
+  external_urls: SpotifyExternalURL;
+  id: string;
+  name: string;
+  preview_url: TPreview;
+}
+
+export interface SpotifyNowPlayingResponse {
+  is_playing: boolean;
+  item: SpotifyTrack;
+}
+
+export interface SpotifyNowPlayingData {
+  albumName: string;
+  artists: string[];
+  image: SpotifyImage | null;
+  previewURL: string | null;
+  title: string;
+  trackID: string;
+  trackURL: string;
+}
+
+export interface SpotifyRecentlyPlayedItem<TPreview = string | null> {
+  played_at: string;
+  track: SpotifyTrack<TPreview>;
+}
+
+export interface SpotifyRecentlyPlayedResponse {
+  items: SpotifyRecentlyPlayedItem[];
+}
+
+export interface SpotifyRecentlyPlayedTrack {
+  albumName: string;
+  artists: string[];
+  id: string;
+  image: SpotifyImage | null;
+  playedAt: string;
+  previewURL: string;
+  title: string;
+  trackURL: string;
+}
+
+export interface SpotifyTopTracksResponse {
+  items: SpotifyTrack[];
+}
+
+export interface SpotifyTopTrackItem
+  extends Omit<SpotifyRecentlyPlayedTrack, 'playedAt'> {
+  rank: number;
+}
