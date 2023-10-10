76600b09faa71b3670fe1fa658e3ab93a4946d40
RofiSyahrul <srofi44@gmail.com>
2023-10-11 06:34:34 +0700

    feat(story): show proper title for toggle audio playing button

--- src/components/stories/components/toggle-audio-playing-button.svelte
+++ src/components/stories/components/toggle-audio-playing-button.svelte
@@ -1,7 +1,26 @@
-<button
-  class="absolute top-0 right-0 bottom-0 left-0 btn btn-text"
-  title="Play/Pause"
-  on:click
->
-  <span class="visually-hidden">Play/Pause</span>
+<script lang="ts">
+  import type Audio from './audio.svelte';
+
+  export let audio: Audio;
+  export let isAudioPlaying: boolean;
+
+  $: title = isAudioPlaying ? 'Pause' : 'Play';
+
+  function handleClick() {
+    audio.toggleAudioPlaying();
+  }
+</script>
+
+<button class="btn btn-text" {title} on:click={handleClick}>
+  <span class="visually-hidden">{title}</span>
 </button>
+
+<style>
+  button {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+  }
+</style>
