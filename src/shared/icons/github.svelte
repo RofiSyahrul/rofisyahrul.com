e36d753e2e731222b9768d5550a955424b0f2ec3
RofiSyahrul <srofi44@gmail.com>
2023-10-11 12:19:40 +0700

    fix(home): portfolio item external links styling

--- src/shared/icons/github.svelte
+++ src/shared/icons/github.svelte
@@ -1,8 +1,14 @@
+<script lang="ts">
+  let className: string | undefined = undefined;
+  export { className as class };
+</script>
+
 <svg
   xmlns="http://www.w3.org/2000/svg"
   viewBox="0 0 24 24"
   width="20"
   height="20"
+  class={className}
 >
   <g fill="currentColor">
     <path
