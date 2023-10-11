cdf7f31c6d1c5a501cb9ced9d63d80adde2b0549
RofiSyahrul <srofi44@gmail.com>
2023-10-11 23:50:14 +0700

    feat(top-track-story): optimize images

--- src/components/stories/top-track.server.ts
+++ src/components/stories/top-track.server.ts
@@ -1,3 +1,4 @@
+import { optimizeImage } from '@/shared/lib/image';
 import { spotifyAPI } from '@/shared/lib/spotify';
 import { storiesConfigMapping } from '@/shared/lib/stories';
 import type { GetServerResponse } from '@/shared/types/general';
@@ -11,19 +12,43 @@ export interface TopTrackStoryProps {
   topTrackStories: TopTrackStoryItem[];
 }
 
+const MAX_IMAGE_SIZE = 300;
+const SIZE_RATIO_PER_RANK = 10;
+
 export const getServerResponse: GetServerResponse<
   TopTrackStoryProps
-> = async ({ cookies, params, redirect }) => {
+> = async ({ cookies, locals, params, redirect }) => {
   const { topTrackSlug = '' } = params;
+  const { isSupportAvif } = locals.userAgent;
 
   const topTracks = await spotifyAPI.getTopTracks();
   if (!topTracks.length) return redirect('/', 307);
 
-  const topTrackStories: TopTrackStoryItem[] = topTracks.map(
-    topTrack => ({
-      slug: `${TOP_TRACK_PREFIX_SLUG}-${topTrack.rank}`,
-      title: `Top Track #${topTrack.rank}`,
-      detail: topTrack,
+  const topTrackStories: TopTrackStoryItem[] = await Promise.all(
+    topTracks.map(async topTrack => {
+      if (topTrack.image?.url) {
+        const size =
+          MAX_IMAGE_SIZE - (topTrack.rank - 1) * SIZE_RATIO_PER_RANK;
+
+        const optimizedImage = await optimizeImage({
+          height: size,
+          isSupportAvif,
+          src: topTrack.image.url,
+          width: size,
+        });
+
+        topTrack.image = {
+          height: size,
+          url: optimizedImage.src,
+          width: size,
+        };
+      }
+
+      return {
+        slug: `${TOP_TRACK_PREFIX_SLUG}-${topTrack.rank}`,
+        title: `Top Track #${topTrack.rank}`,
+        detail: topTrack,
+      };
     }),
   );
 
