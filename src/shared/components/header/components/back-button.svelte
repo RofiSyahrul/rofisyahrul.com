662877f2a00882a591e77edc0c0f38eeeb689741
RofiSyahrul <srofi44@gmail.com>
2023-10-11 18:45:20 +0700

    refactor(header): reduce tailwind classes

--- src/shared/components/header/components/back-button.svelte
+++ src/shared/components/header/components/back-button.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
   import goBack from '@/shared/lib/client/go-back';
 
-  export let title = '';
+  export let title: string;
 </script>
 
 <button
-  class="btn btn-text py-2 px-0 umami--click--header__go-back"
+  class="btn btn-text umami--click--header__go-back"
   {title}
   on:click={goBack}
 >
   <slot />
 </button>
+
+<style>
+  .btn {
+    padding: 8px 0;
+  }
+</style>
