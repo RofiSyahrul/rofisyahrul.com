7e2de2bf48eb948e7fbca07985d4355373a47602
RofiSyahrul <srofi44@gmail.com>
2023-10-11 22:37:25 +0700

    refactor: reduce tailwindcss classes in soon and moon icons

--- src/shared/icons/sun.svelte
+++ src/shared/icons/sun.svelte
@@ -1,5 +1,12 @@
+<script lang="ts">
+  let className = '';
+  export { className as class };
+</script>
+
 <svg
-  class="w-5 h-5 hidden dark:block"
+  class={className}
+  height="20"
+  width="20"
   viewBox="0 0 20 20"
   xmlns="http://www.w3.org/2000/svg"
 >
